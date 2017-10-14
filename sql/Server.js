00e3c6a176e156cbe2cff42dd88ec5da22ad521e
Asimm Hirani <ahh200247718@gmail.com>
2017-10-14 05:30:45 -0400

    cleaning up server for next stage

--- sql/Server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-var staticContentDir = '../content/static_html/'
-
-app.get('/',function(req,res) {
-  res.sendFile(staticContentDir+'index.html');
-});
-app.listen(3000); //the port you want to use
