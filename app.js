a7e3befed1d0e2e52584d02fb649e79def2aa3a5
Asimm Hirani <ahh200247718@gmail.com>
2017-10-14 02:15:49 -0400

    Getting SQL comms working

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
+var sqlTest = require('./sql/sqlTest.js');
+
 var staticContentDir = 'content/static_html/';
 
 app.use(express.static(staticContentDir));
+
+app.get('/sqlTest', function (req, res) {
+
+})
 
 
 app.listen(process.env.PORT || 3000); //the port you want to use
